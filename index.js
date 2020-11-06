3fa57e1844d2453efb8bbab938102f3f7c62ea9e
Camila Kikuchi <camila_kikuchi@yahoo.com.br>
2020-11-06 13:21:13 -0300

    mensagem do commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+
+
+
+    const btn1 = document.getElementById("btn1")
+    btn1.addEventListener("click", cipher.decode);
+
+    const btn = document.getElementById("btn")
+    btn.addEventListener("click", cipher.encode);
+
+    
+    
+    
+    
+    
+    
