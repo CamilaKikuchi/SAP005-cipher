d6af7ff87b62a61e686f878a398f61b0da713182
Camila Kikuchi <71717826+CamilaKikuchi@users.noreply.github.com>
2020-11-06 18:26:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import cipher from './cipher.js';
 
 
     const btn1 = document.getElementById("btn1")
+    
     btn1.addEventListener("click", cipher.decode);
 
     const btn = document.getElementById("btn")
+    
     btn.addEventListener("click", cipher.encode);
 
     
